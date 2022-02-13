166968086f374065eda3357327372662f29805a8
İlayda Kuran <66517728+ilaydakuran@users.noreply.github.com>
2022-02-13 22:58:41 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,84 @@
+import 'package:cs310_week5_app/routes/editprofile.dart';
+import 'package:cs310_week5_app/routes/profile.dart';
+import 'package:cs310_week5_app/routes/search.dart';
+import 'package:cs310_week5_app/routes/welcome.dart';
 import 'package:flutter/material.dart';
+import 'package:cs310_week5_app/routes/login.dart';
+import 'package:cs310_week5_app/routes/home.dart';
+import 'package:cs310_week5_app/routes/signup.dart';
+import 'package:cs310_week5_app/routes/walkthrough.dart';
+import 'package:cs310_week5_app/routes/bildirim.dart';
+import 'package:cs310_week5_app/routes/nots_model.dart';
+import 'package:cs310_week5_app/routes/nots.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
-  runApp(ChipView());
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
 }
+class MyApp extends StatelessWidget {
 
-class ChipView extends StatelessWidget {
-  const ChipView({key}) : super(key: key);
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-
-        children: [
-          Chip(label: Text('I')),
-          Chip(label: Text('really')),
-          Chip(label: Text('really')),
-          Chip(label: Text('really')),
-          Chip(label: Text('really')),
-          Chip(label: Text('really')),
-          Chip(label: Text('really')),
-          Chip(label: Text('need')),
-          Chip(label: Text('a')),
-          Chip(label: Text('good')),
-          Chip(label: Text('grade')),
-        ]);
-
-
-  }
-}
-/*
-class Wrapper extends StatelessWidget {
-
-  //Initialize firebase here
-  //final Future<FirebaseApp> _initialization =Firebase.initializeApp();
-
-  @override
-  Widget build(BuildContext context) {
-
-    //Handle firebase connectivity here
     return FutureBuilder(
-      future: _initialization,
-      builder: (context.snapshot){,
-        if(snapshot.hasError){
-          print('Firebase not connected' +snapshot.error);
+        future: _initialization,
+        builder: (context, snapshot) {
+          if(snapshot.hasError) {
+            print('Cannot connect to firebase: '+snapshot.error);
+            return MaterialApp(
+              home: Welcome(),
+            );
+          }
+          if(snapshot.connectionState == ConnectionState.done) {
+            print('Firebase connected');
+            return AppBase();
+          }
+
           return MaterialApp(
-            home: NoFireBaseView(),
+            home: Welcome(),
           );
-    }
-    if(snapshot.connectionState== ConnectionState.done){
-      print('Firebase connected');
-      return StreamProvider<User>.value(
-        value.AuthService().user,
-        child: MaterialApp(
-          home: Authenticate();
-        ),
-      );
-    }
-    return MaterialApp(
-        print('Waiting for response' +snapshot.error);
-      home: WaitingView();
-    );
-
-
-    }
+        }
     );
   }
 }
-*/
+class AppBase extends StatelessWidget {
+  const AppBase({
+    Key key,
+  }) : super(key: key);
+
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(analytics: analytics);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+        navigatorObservers: <NavigatorObserver>[observer],
+      //  home: HomePage(analytics: analytics, observer: observer),
+    routes: {
+    //'/': (context) => TestScreen(),
+    '/': (context) => Welcome(),
+    '/login': (context) => Login(),
+    '/signup': (context) => SignUp(),
+    '/walkthrough': (context) => TestScreen(),
+    '/home': (context) => HomePage(analytics: analytics, observer: observer),
+    '/profile': (context) => ProfilePage(),
+    '/search': (context) => Search(),
+    '/editprofile' :(context) => EditProfilePage(),
+    '/bildirim' :(context) => bildirim(),
+
+    },
+    );
+  }
+}  //home: Welcome(),
+  //initialRoute: '/walkthrough',
+ // initialRoute: '/home',
+
+
 
